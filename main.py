24355e23413fe5396f2c9aecb9162a2d66d1d876
Halil_Hasmer <halilpython@gmail.com>
2023-08-25 22:00:40 +0300

    create basic skeleton

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print()
+from flask.views import MethodView
+from wtforms import Form
+
+
+class HomePage(MethodView):
+    pass
+
+
+class BillFormPage(MethodView):
+    pass
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(Form):
+    pass
